d13fa6590239feb300765a47fcd6ae6b3078cbc9
Victor Avila <avilavictor1570@gmail.com>
2020-07-30 02:55:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,14 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :email
+  attr_accessor :name, :csv_emails
+
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
+  
+  def parse
+    csv_emails.split.collect do |address|address.split(" ")
+  end
+  .flatten.unique
 end
